9fb290e45e0d8c2b44c91b8779d721dff9d045c1
SilviaCastro26 <scastroc@fpcoruna.afundacion.com>
2021-12-17 12:14:08 +0100

    Ejercicio 3 parte 1

--- src/components/product-item/ProductItem.tsx
+++ src/components/product-item/ProductItem.tsx
@@ -1,12 +1,18 @@
 import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 import Counter from "../counter/Counter";
 import useStyles from "./ProductItem-styles";
 
 const ProductItem = (props: any) => {
+    const navigate = useNavigate();
     const classes = useStyles();
 
+    const onClickButton = () => {
+        navigate('details');
+    };
+
     return (
-        <div className={classes.card}>
+        <div className={classes.card} onClick={onClickButton}>
             <div className={classes.imageContainer}>
                 <img className={classes.image} src={props.product.image} />
             </div>
